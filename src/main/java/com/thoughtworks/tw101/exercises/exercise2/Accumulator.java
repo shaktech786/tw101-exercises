58861d015bd4ec05d51c6ef5cb6dbbe451036932
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2016-07-25 17:58:12 -0400

    accumulator

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    private int total;
+
+    public void increment(){
+        total++;
+    }
+
+    public int total() {
+        return total;
+    }
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         Accumulator accumulator = new Accumulator();
-
+        for (int i = 0; i < 5; i++) {
+            accumulator.increment();
+        }
+        System.out.println(accumulator.total());
 
     }
 }
