08d5b60e9906ffaa8ccdb0f9df640b8f0bf82333
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2016-07-25 14:13:50 -0400

    Hello World successful

---
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.tw101.introductory_programming_exercises;
+
+/**
+ * Created by sbhamani on 7/25/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
