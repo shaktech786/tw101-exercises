00d6c9446313d0e9df220edc99c9347644a5a479
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2016-08-03 12:20:26 -0400

    Figured out the BST implementation!

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,49 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
+    private Node right, left;
     private String name;
+    private List<String> namesList;
 
     public Node(String name) {
         this.name = name;
+        this.left = null;
+        this.right = null;
+        namesList = new ArrayList<>();
     }
 
     public void add(String nameOfNewNode) {
+        if (nameOfNewNode.compareTo(name) < 1) {
+            if (this.left == null) {
+                this.left = new Node(nameOfNewNode);
+                return;
+            } else {
+                this.left.add(nameOfNewNode);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = new Node(nameOfNewNode);
+                return;
+            } else {
+                this.right.add(nameOfNewNode);
+            }
+        }
     }
 
     public List<String> names() {
-        return null;
+        inOrderTraversal(this);
+        return namesList;
+    }
+
+    private void inOrderTraversal(Node root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            namesList.add(root.name);
+            inOrderTraversal(root.right);
+        }
     }
 }
